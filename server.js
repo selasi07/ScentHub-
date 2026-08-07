7a5695527c40bc56a7b9142f8c326e1a7941fe6c
kellyadoboe1 <kkadoboe001@st.ug.edu.gh>
2026-08-07 14:02:03 +0000

    Connect Express to frontend directory

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/products", productRoutes);
 
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "public", "index.html"));
+    res.sendFile(path.join(__dirname, "public", "frontend", "index.html"));
 });
 
 app.listen(PORT, "0.0.0.0", () => {
